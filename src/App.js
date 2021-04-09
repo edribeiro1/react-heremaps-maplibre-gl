391a4b4d0e0bf64fafdecccbc0383e35ba5fd36f
Edson Ribeiro <edson.ribeiro@ftdata.com.br>
2021-04-09 11:24:46 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useRef} from 'react';
+import {Map, NavigationControl, GeolocateControl} from 'maplibre-gl';
+import vetorial from './maps-styles/vetorial.json';
+import hybrid from './maps-styles/hybrid.json';
+import hybrid_maptiler from './maps-styles/hybrid-maptiler.json';
 
-function App() {
+function App () {
+  const mapContainerRef = useRef ();
+  // const mapTilerKey = "get_your_own_OpIi9ZULNHzrESv6T2vL"
+
+  useEffect (() => {
+    if (mapContainerRef.current != null) {
+      const map = new Map ({
+        container: mapContainerRef.current,
+        center: [-77.517893, -6.614297],
+        zoom: 2,
+        dragRotate: false,
+        // `https://assets.vector.hereapi.com/styles/berlin/base/mapbox/tilezen?apikey=LF62UDMv0QGJMH6Z_YScIaHJ1CxCyTiBYFpPzNxZtzI`
+        // `https://vector.hereapi.com/v2/vectortiles/base/mc?apikey=LF62UDMv0QGJMH6Z_YScIaHJ1CxCyTiBYFpPzNxZtzI`
+        style: hybrid,
+      });
+
+      map.addControl (new NavigationControl (), 'top-left');
+      map.scrollZoom.setWheelZoomRate(1);
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+
+      ref={mapContainerRef}
+      style={{
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        width: "100%"
+      }}
+    />
   );
 }
 
